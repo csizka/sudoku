b3a2c1df947498a23e252f5504766a2f4d281a2f
csizka <csizmadia.kata@gmail.com>
2024-02-25 22:15:53 +0100

    port some asserts to tests

--- src/test/scala/sudoku/ValidationTest.scala
+++ src/test/scala/sudoku/ValidationTest.scala
@@ -1,12 +1,22 @@
 package junicamp
 package sudoku
 
-import sudoku.Validation._
+import sudoku.Examples.*
+import sudoku.Validation.*
 
-import utest._
+import utest.*
 
-object ValidationTest {
-  def main(args: Array[String]) = {
-    assert(!areCellsSolved(Vector(1, 2, 3, 4, 5, 6, 7, 8, 9, 10).map(x => Some(x))))
+object ValidationTest extends TestSuite {
+  val tests = Tests {
+    test("areCellsSolved") {
+      val filledButInvalidRow = Vector(1, 2, 3, 4, 5, 6, 7, 8, 9, 10).map(x => Some(x))
+      assert(!areCellsSolved(filledButInvalidRow))
+    }
+
+    test("countSolutions") {
+      countSolutions(oneCellMissingSudoku) ==> 1
+      countSolutions(partiallySolvedGoodSudoku) ==> 1
+      countSolutions(goodSudoku) ==> 1
+    }
   }
 }
