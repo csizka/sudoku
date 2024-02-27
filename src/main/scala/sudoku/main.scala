477d0d69f21045418b1e950769ff07a683eda36d
csizka <csizmadia.kata@gmail.com>
2024-02-27 23:03:30 +0100

    add example for file IO

--- src/main/scala/sudoku/main.scala
+++ src/main/scala/sudoku/main.scala
@@ -5,12 +5,25 @@ import Sudoku.*
 import Examples.*
 import Pretty.*
 import sudoku.Validation.*
+
 import Generate.*
 import Solving.*
 
 import scala.util.Random
+import java.nio.file.{Files, Paths}
+import scala.jdk.CollectionConverters._
 
 @main
 def main(): Unit = {
-  
+  val path = Paths.get("./test.txt")
+  Files.write(path, List("Hello world!", "second line").asJava)
+
+  val lines = Files.readAllLines(path).asScala
+  println(lines)
+
+  val wholeFileAsString = Files.readString(path)
+  println(wholeFileAsString)
+
+  val bytes = Files.readAllBytes(path).toList
+  println(bytes)
 }
